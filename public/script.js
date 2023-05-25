78ac2aad6edade1c950ba3e49969431774422147
danny 10-2 <dnioko0412@gmail.com>
2023-05-25 14:47:50 +0800

    v1.0

--- public/script.js
+++ public/script.js
@@ -1,18 +1,18 @@
-const text = document.querySelector("its-me");
-const textload = () => {
-  setTimeout(() => {
-    text.textContent = "Danny";
-  }, 0);
-  setTimeout(() => {
-    text.textContent = "Дэнни";
-  }, 4000);
-  setTimeout(() => {
-    text.textContent = "Programmer";
-  }, 8000);
-};
-textload();
+// const text = document.querySelector("its-me");
+// const textload = () => {
+//   setTimeout(() => {
+//     text.textContent = "Danny";
+//   }, 0);
+//   setTimeout(() => {
+//     text.textContent = "Дэнни";
+//   }, 4000);
+//   setTimeout(() => {
+//     text.textContent = "Programmer";
+//   }, 8000);
+// };
+// textload();
 
-setInterval(textload, 12000);
+// setInterval(textload, 12000);
 
 
 
@@ -40,6 +40,9 @@ function edu() {
 function exp() {
   location.href = "#experience"
 }
+function cont(){
+  location.href = "#p10"
+}
 
 function huviinhereghmtl() {
   window.open = "./elementaryschool.html/index.html"
@@ -51,8 +54,18 @@ function phonemarket(){
   window.open("https://phonemarket-ed42e.web.app/index.html")
 }
 function intern(){
-  window.open("./Дадлагажигч Demo")
+  window.open("./Дадлагажигч Demo/index.html")
 }
 function biology(){
-  window.open("../biology biy daalt")
+  window.open("./biology biy daalt/index.html")
 }
+
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('active');
+}
+
+const indexImg = document.querySelector(".start-img");
+
+indexImg.style.opacity = '1'
+indexImg.style.transition = '2s'
